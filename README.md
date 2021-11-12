5d250fe28c8ecda6e4c292ba6383ecb76c05c6a5
kml01 <kml_brar01@yahoo.com>
2021-11-12 10:21:11 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hxqpga-uraemg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hxqpga-uraemg)
---
+++ src/app/students.ts
@@ -0,0 +1,27 @@
+export interface Student {
+  id: number;
+  firstName: string;
+  lastName: number;
+  email: string;
+}
+
+export const students = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Connor',
+    email: 'johnconnor@gmail.com',
+  },
+  {
+    id: 2,
+    firstName: 'Sarah',
+    lastName: 'Connor',
+    email: 'sarahconnor@gmail.com',
+  },
+  {
+    id: 3,
+    firstName: 'Kyle',
+    lastName: 'Reese',
+    email: 'kyle@gmail.com',
+  },
+];
